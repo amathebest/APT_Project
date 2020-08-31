a42d97aaaf4ee0fb19d41f7469a79ecc70126096
amathebest <amatori.matt@gmail.com>
2020-08-31 13:26:20 +0200

    Added model class and setup jacoco and sonarcloud

---
+++ com.project.apt.gestionale/src/main/java/com/project/apt/model/Product.java
@@ -0,0 +1,49 @@
+package com.project.apt.model;
+
+import java.util.Objects;
+
+public class Product {
+	private String name;
+	private int quantity;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Product other = (Product) obj;
+		return Objects.equals(name, other.name);
+	}
+	
+	@Override
+	public String toString() {
+		return "Product [Name=" + name + ", quantity=" + quantity + "]";
+	}
+}
