46f0d081fb389b28b6de031a1f6449a99d1168a6
amathebest <amatori.matt@gmail.com>
2020-09-08 17:26:19 +0200

    Cleaned some code smells 5

--- com.project.apt.gestionale/src/main/java/com/project/apt/controller/ProductController.java
+++ com.project.apt.gestionale/src/main/java/com/project/apt/controller/ProductController.java
@@ -7,6 +7,8 @@ import com.project.apt.view.ProductView;
 public class ProductController {
 	
 	private static final String NO_PRODUCT_FOUND_ERROR = "No such product existing in the database";
+	private static final String ERROR_KEY = "error";
+	private static final String INFO_KEY = "info";
 	
 	private ProductView productView;
 	private ProductRepository productRepository;
@@ -26,23 +28,23 @@ public class ProductController {
 			productRepository.alterProductQuantity(existingProduct, existingProduct.getQuantity() + product.getQuantity());
 			Product updatedProduct = new Product(product.getName(), existingProduct.getQuantity() + product.getQuantity());
 			productView.productEdited(existingProduct, updatedProduct);
-			productView.showError("Updating quantity", updatedProduct, "info");
+			productView.showError("Updating quantity", updatedProduct, INFO_KEY);
 			return;
 		}
 		productRepository.addProduct(product);
 		productView.productAdded(product);
-		productView.showError("Product added", product, "info");
+		productView.showError("Product added", product, INFO_KEY);
 	}
 	
 	public void removeProduct(Product product) {
 		Product existingProduct = productRepository.findByName(product.getName());
 		if (existingProduct == null) {
-			productView.showError(NO_PRODUCT_FOUND_ERROR, product, "error");
+			productView.showError(NO_PRODUCT_FOUND_ERROR, product, ERROR_KEY);
 			return;
 		}
 		productRepository.removeProduct(product);
 		productView.productDeleted(product);
-		productView.showError("Product removed", product, "info");
+		productView.showError("Product removed", product, INFO_KEY);
 	}
 	
 	public void updateProductName(Product product, String newName) {
@@ -53,24 +55,24 @@ public class ProductController {
 				productRepository.alterProductName(product, newName);
 				Product updatedProduct = new Product(newName, product.getQuantity());
 				productView.productEdited(product, updatedProduct);
-				productView.showError("Changing product name", updatedProduct, "info");
+				productView.showError("Changing product name", updatedProduct, INFO_KEY);
 				return;
 			}
-			productView.showError("Database contains already a product with selected name", product, "error");
+			productView.showError("Database contains already a product with selected name", product, ERROR_KEY);
 			return;
 		}
-		productView.showError(NO_PRODUCT_FOUND_ERROR, product, "error");
+		productView.showError(NO_PRODUCT_FOUND_ERROR, product, ERROR_KEY);
 	}
 	
 	public void updateProductQuantity(Product product, int newQuantity) {
 		Product existingProduct = productRepository.findByName(product.getName());
 		if (existingProduct == null) {
-			productView.showError(NO_PRODUCT_FOUND_ERROR, product, "error");
+			productView.showError(NO_PRODUCT_FOUND_ERROR, product, ERROR_KEY);
 			return;
 		}
 		productRepository.alterProductQuantity(product, newQuantity);
 		productView.productEdited(product, new Product(product.getName(), newQuantity));
 		Product updatedProduct = new Product(product.getName(), newQuantity);
-		productView.showError("Changing product quantity", updatedProduct, "info");
+		productView.showError("Changing product quantity", updatedProduct, INFO_KEY);
 	}
 }
--- com.project.apt.gestionale/src/main/java/com/project/apt/repository/ProductRepositoryMongo.java
+++ com.project.apt.gestionale/src/main/java/com/project/apt/repository/ProductRepositoryMongo.java
@@ -14,7 +14,10 @@ import com.project.apt.model.Product;
 public class ProductRepositoryMongo implements ProductRepository {
 
 	private MongoCollection<Document> productDocCollection;
-
+	
+	private static final String NAME_KEY = "name";
+	private static final String QUANTITY_KEY = "quantity";
+	
 	public ProductRepositoryMongo(MongoClient client, String dbGestionale, String productCollection) {
 		productDocCollection = client.getDatabase(dbGestionale).getCollection(productCollection);
 	}
@@ -26,7 +29,7 @@ public class ProductRepositoryMongo implements ProductRepository {
 
 	@Override
 	public Product findByName(String name) {
-		Document productDoc = productDocCollection.find(Filters.eq("name", name)).first();
+		Document productDoc = productDocCollection.find(Filters.eq(NAME_KEY, name)).first();
 		if (productDoc != null) {
 			return fromDocumentToProduct(productDoc);
 		}
@@ -35,30 +38,30 @@ public class ProductRepositoryMongo implements ProductRepository {
 
 	@Override
 	public void addProduct(Product product) {
-		productDocCollection.insertOne(new Document().append("name", product.getName()).append("quantity", product.getQuantity()));
+		productDocCollection.insertOne(new Document().append(NAME_KEY, product.getName()).append(QUANTITY_KEY, product.getQuantity()));
 	}
 
 	@Override
 	public void removeProduct(Product product) {
-		productDocCollection.deleteOne(Filters.eq("name", product.getName()));
+		productDocCollection.deleteOne(Filters.eq(NAME_KEY, product.getName()));
 	}
 
 	@Override
 	public void alterProductName(Product product, String newName) {
-		Document docToUpdate = new Document().append("name", product.getName()).append("quantity", product.getQuantity());
-		Document docToReplace = new Document().append("name", newName).append("quantity", product.getQuantity());
+		Document docToUpdate = new Document().append(NAME_KEY, product.getName()).append(QUANTITY_KEY, product.getQuantity());
+		Document docToReplace = new Document().append(NAME_KEY, newName).append(QUANTITY_KEY, product.getQuantity());
 		productDocCollection.replaceOne(docToUpdate, docToReplace);
 	}
 
 	@Override
 	public void alterProductQuantity(Product product, int newQuantity) {
-		Document docToUpdate = new Document().append("name", product.getName()).append("quantity", product.getQuantity());
-		Document docToReplace = new Document().append("name", product.getName()).append("quantity", newQuantity);
+		Document docToUpdate = new Document().append(NAME_KEY, product.getName()).append(QUANTITY_KEY, product.getQuantity());
+		Document docToReplace = new Document().append(NAME_KEY, product.getName()).append(QUANTITY_KEY, newQuantity);
 		productDocCollection.replaceOne(docToUpdate, docToReplace);
 	}
 	
 	private Product fromDocumentToProduct(Document doc) {
-		return new Product("" + doc.get("name"), (int) doc.get("quantity"));
+		return new Product("" + doc.get(NAME_KEY), (int) doc.get(QUANTITY_KEY));
 	}
 
 }
